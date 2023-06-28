3003924698f8b3a59ba78f7d8e61027cf6d3bb28
thatguyKacper <kacper.m.wojtowicz@gmail.com>
2023-06-28 15:58:40 +0200

    Revert "Merge branch 'Backend' into Frontend"
    This reverts commit a98ea93fa92d46b4ef1991155204199ed2249495.

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,28 +1,10 @@
-import Nav from './components/Nav';
-import Sidebar from './components/Sidebar';
-import Dashboard from './pages/Dashboard';
-import Users from './pages/Users';
-import Boardgames from './pages/Boardgames';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Boardgames from './boardgames/Boardgames';
 
 function App() {
   return (
     <>
-      <Nav />
-
-      <div className="container-fluid">
-        <div className="row">
-          <Sidebar />
-
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <Routes>
-              <Route exact path="/" element={<Dashboard />} />
-              <Route path="/boardgames" element={<Boardgames />} />
-              <Route path="/users" element={<Users />} />
-            </Routes>
-          </main>
-        </div>
-      </div>
+      <p>Hello from React</p>
+      <Boardgames />
     </>
   );
 }
