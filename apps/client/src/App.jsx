4c0d73f0b2005090e62629f4b422c3da8cadb2ce
thatguyKacper <kacper.m.wojtowicz@gmail.com>
2023-06-28 15:45:19 +0200

    Add bootstrap & router & some basic layout

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,10 +1,28 @@
-import Boardgames from './boardgames/Boardgames';
+import Nav from './components/Nav';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
+import Boardgames from './pages/Boardgames';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <p>Hello from React</p>
-      <Boardgames />
+      <Nav />
+
+      <div className="container-fluid">
+        <div className="row">
+          <Sidebar />
+
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route path="/boardgames" element={<Boardgames />} />
+              <Route path="/users" element={<Users />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
     </>
   );
 }
---
+++ apps/client/src/components/Search.jsx
@@ -0,0 +1,12 @@
+export default function Search() {
+  return (
+    <>
+      <input
+        className="form-control form-control-dark w-100 rounded-0 border-0"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+      />
+    </>
+  );
+}
